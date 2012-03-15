753f22a35ad6ccc76c15db8b56865dcb21b60ee0
nicolasbrailo <nicolasbrailo@gmail.com>
2012-03-15 14:00:27 +0100

    Added config file for the grepwrapper

--- fastgrep.sh
+++ fastgrep.sh
@@ -1,24 +1,5 @@
 #!/bin/bash
 
-INDEX_DIRS="client sandbox src test www"
-EXCLUDE_DIRS="www/static"
-
-
-# Given a file name, will iterate up the directory tree until
-# the file is found
-function find_file_in_tree() {
-    file=$1
-    dir=`pwd`
-    while [ ${#dir} -gt 1 ]; do
-        if [ -e $dir/$file ]; then
-            echo "$dir/$file";
-            break;
-        fi
-        dir=`dirname $dir`
-    done
-}
-
-
 #####################################################
 # Cache building functions
 #####################################################
@@ -29,12 +10,25 @@ BASE_IGNORE_REGEX='\.git|\.svn|\.jpg|\.png|\.pdf|\.doc|\.ttf|\.pyc'
 # Lists all the file in the project, filtering out the binaries and the
 # set of user defined uninteresting files
 function list_interesting_files() {
-    for dir in $INDEX_DIRS; do
+    dirs_to_check=$1
+    exclude_pattern=$2
+
+    if [ ${#dirs_to_check} -eq 0 ]; then
+        dirs_to_check=`ls .`
+    fi
+
+    if [ ${#exclude_pattern} -gt 1 ]; then
+        pattern="$BASE_IGNORE_REGEX|$exclude_pattern";
+    else
+        pattern="$BASE_IGNORE_REGEX";
+    fi
+
+    for dir in $dirs_to_check; do
         # find all the files in the interesting dirs
         #       | grep out the file types we don't care about
         #       | get file info for each one
         #       | grep those that contain text
-        find $dir -type f | egrep -v "$BASE_IGNORE_REGEX|$EXCLUDE_DIRS" \
+        find $dir -type f | egrep -v "$pattern" \
                 | xargs file | grep 'text' | awk -F':' '{print $1}'
     done
 }
@@ -43,8 +37,11 @@ function list_interesting_files() {
 # by the 'list_interesting_files' function
 function rebuild_cache() {
     grepcache_file=$1
+    dirs_to_check=$2
+    exclude_pattern=$3
+
     echo '' > $grepcache_file
-    for file in `list_interesting_files`; do
+    for file in `list_interesting_files $dirs_to_check $exclude_pattern`; do
         cat $file | awk "{print \"$file \"\$0}" >> $grepcache_file
     done
 }
@@ -85,13 +82,64 @@ function wraped_grep() {
 
 
 #####################################################
+# Random stuff
+#####################################################
+
+# Given a file name, will iterate up the directory tree until
+# the file is found
+function find_file_in_tree() {
+    file=$1
+    dir=`pwd`
+    while [ ${#dir} -gt 1 ]; do
+        if [ -e $dir/$file ]; then
+            echo "$dir/$file";
+            break;
+        fi
+        dir=`dirname $dir`
+    done
+}
+
+# Write a new config file
+function rebuild_config() {
+    old_idx_dirs=$1
+    new_idx_dirs=$2
+    old_excl_ptn=$3
+    new_excl_ptn=$4
+    config_path=$5
+
+    if [ "${#new_idx_dirs}" -eq 0 ]; then
+        new_idx_dirs=$old_idx_dirs
+    fi
+
+    if [ "${#new_excl_ptn}" -eq 0 ]; then
+        new_excl_ptn=$old_excl_ptn
+    fi
+
+    echo "INDEX_DIRS=\"$new_idx_dirs\"" > $config_path
+    echo "EXCLUDE_PATTERN=\"$new_excl_ptn\"" >> $config_path
+}
+
+#####################################################
 # User interface
 #####################################################
 
 
-GREPCACHE_BASE_FILE=.grepcache
+INDEX_DIRS=""
+EXCLUDE_PATTERN=""
 
-while getopts "hr" opt; do
+GREPCACHE_CONFIG_BASE_FILE=.grepcacheconfig
+GREPCACHE_CONFIG_FILE=`find_file_in_tree $GREPCACHE_CONFIG_BASE_FILE`
+
+GREPCACHE_BASE_FILE=.grepcache
+GREPCACHE_FILE=`find_file_in_tree $GREPCACHE_BASE_FILE`
+
+# If available load the config file
+if [ "${#GREPCACHE_CONFIG_FILE}" -gt 2 ]; then
+    source $GREPCACHE_CONFIG_FILE
+fi
+
+
+while getopts "chr" opt; do
     case "$opt" in
         h) echo -ne "$0 is a simple grep wrapper to speed up searches in a large"
            echo -ne " set of files. If you find yourself running 'grep -r *' and"
@@ -102,16 +150,26 @@ while getopts "hr" opt; do
            echo "Run options:"
            echo "  -h: This help"
            echo "  -r: Rebuild cache"
+           echo "  -c: Configure and rebuild (eg set exclude patterns)"
            echo ""
            exit ;;
+        c) echo "Reconfiguring cache options..."
+           echo -n "Type a space separated list of the directories to "
+           echo "index, then enter [$INDEX_DIRS]:"
+           echo -n " > "
+           read new_dirs_to_index
+           echo "Type a grep style exclusion pattern, then enter [$EXCLUDE_PATTERN]:"
+           echo -n " > "
+           read new_exclude_pattern
+           rebuild_config "$INDEX_DIRS" "$new_dirs_to_index" \
+                          "$EXCLUDE_PATTERN" "$new_exclude_pattern" \
+                          $GREPCACHE_CONFIG_BASE_FILE
+           exit ;;
         r) echo "Rebuilding cache...";
-           rebuild_cache ./$GREPCACHE_BASE_FILE
+           rebuild_cache ./$GREPCACHE_BASE_FILE $INDEX_DIRS $EXCLUDE_PATTERN
            exit ;;
     esac
 done
-
-
-GREPCACHE_FILE=`find_file_in_tree $GREPCACHE_BASE_FILE`
 
 if [ "${#GREPCACHE_FILE}" -lt 2 ]; then
     echo "Cache file $GREPCACHE_BASE_FILE not found."
@@ -119,5 +177,5 @@ if [ "${#GREPCACHE_FILE}" -lt 2 ]; then
     exit
 fi
 
-wraped_grep GREPCACHE_FILE $1
+wraped_grep $GREPCACHE_FILE $1
 

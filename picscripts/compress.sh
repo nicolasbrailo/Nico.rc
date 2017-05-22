1573fc3dc62c5d1902a9406fa52703822a627b27
Nicolas Brailovsky <nicolasbrailo@gmail.com>
2017-05-22 20:44:11 +0200

    Improved picompress

--- picscripts/compress.sh
+++ picscripts/compress.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
 TGT_EXT=JPG
+SUBDIR=compressed
+ORIGINALSDIR=originals
 FILES=`ls *.$TGT_EXT`
 
 echo "Press enter to compress the following files:"
 echo $FILES
 read -p ""
+mkdir -p $SUBDIR
+mkdir -p $ORIGINALSDIR
 
 for i in $FILES
 do
     BASE_NAME=`basename $i .$TGT_EXT`
-    NEW_NAME=$BASE_NAME"_v2.JPG"
+    NEW_NAME=$SUBDIR"/"$BASE_NAME".JPG"
     echo convert -quality 85 $i $NEW_NAME
     convert -quality 85 $i $NEW_NAME
+    mv $i $ORIGINALSDIR
 done
+
+mv $SUBDIR/* .
+rm -rf $SUBDIR
+du -h
+
 
